25c788675dc17ac5223be12d309b5e8056b394cd
Oğuzhan Kandakoğlu <47363718+Ouzx@users.noreply.github.com>
2021-12-10 23:30:40 +0300

    flow changed

---
+++ src/Sideflow/CategoryScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const CategoryScreen = () => {
+  return <View></View>;
+};
+
+export default CategoryScreen;
+
+const styles = StyleSheet.create({});
